4b665c23ddcd91a5cedbc339adc049a0dfdc3a99
Aaron Lewandowski <a.lewandowski@tks.eu>
2023-12-06 15:28:19 +0100

    fix: init with GLOBAL_FLAGS

--- manifests/repository.pp
+++ manifests/repository.pp
@@ -198,12 +198,16 @@ define restic::repository (
     },
   }
 
+  $config_keys = {
+    'GLOBAL_FLAGS' => [ $_global_flags, ].flatten.join(' '),
+  } + $type_config
+
   if $_init_repo {
     exec { "restic_init_${title}":
       command     => "${_binary} init",
       # TODO: Make environment a Sensitive data type as well. But this is currently not supported by Puppet
       # https://github.com/puppetlabs/puppet/blob/f4781e349d0089061d05f1e8e6e3c3d44a7e1e04/lib/puppet/type/exec.rb#L692
-      environment => $type_config.reduce([]) |$memo,$item| { $memo + "${item[0]}=${item[1].unwrap}" }.sort,
+      environment => $config_keys.reduce([]) |$memo,$item| { $memo + "${item[0]}=${item[1].unwrap}" }.sort,
       onlyif      => "${_binary} snapshots 2>&1 | grep -q 'Is there a repository at the following location'",
     }
   }
@@ -217,10 +221,6 @@ define restic::repository (
       owner          => 'root',
       show_diff      => true,
     }
-
-    $config_keys = {
-      'GLOBAL_FLAGS' => [ $_global_flags, ].flatten.join(' '),
-    } + $type_config
 
     $config_keys.each |$config,$data| {
       concat::fragment { "restic_fragment_${title}_${config}":
